90580c9ee99d265ce75ea15ef7e6128d3cf08a76
Jay Gordon <jaydestro@gmail.com>
2020-04-24 14:33:04 -0400

    Update aks-acr.sh

--- aks-acr.sh
+++ aks-acr.sh
@@ -4,6 +4,7 @@ GROUPNAME=""
 LOCATION=""
 AKSNAME=""
 ACRNAME="" # must be globally unique
+SUBID=""
 
 ### get version by running
 ### az aks get-versions --location $LOCATION --output table
@@ -24,7 +25,7 @@ echo $PASS
 sleep 5
 
 # create resource group based on groupname variable
-az group create --name $GROUPNAME --location $LOCATION
+az group create --subscription $SUBID --name $GROUPNAME --location $LOCATION
 
 sleep 2
 
@@ -32,31 +33,31 @@ sleep 2
 
 echo "Creating cluster, this could take a few mins"
 
-az aks create --resource-group $GROUPNAME --name $AKSNAME \
+az aks create --subscription $SUBID --resource-group $GROUPNAME --name $AKSNAME \
 --enable-addons monitoring,http_application_routing \
 --kubernetes-version $k8sversion  --generate-ssh-keys \
 --service-principal $APPID --client-secret $PASS
 
 # get the aks cluster pass for kubectl access
 
-az aks get-credentials --resource-group $GROUPNAME --n $AKSNAME
+az aks get-credentials --subscription $SUBID --resource-group $GROUPNAME --n $AKSNAME
 
 echo "cluster creds for are imported"
 
 # create an azure container registry for images, admin enabled for docker login
 
-az acr create --resource-group $GROUPNAME --name $ACRNAME --sku Standard --admin-enabled true
+az acr create --subscription $SUBID --resource-group $GROUPNAME --name $ACRNAME --sku Standard --admin-enabled true
 
 # creating RBAC for Azure AD to permit build and pull from acr to aks cluster
 
 # Get the id of the service principal configured for AKS
-CLIENT_ID=$(az aks show --resource-group $GROUPNAME --name $AKSNAME --query "servicePrincipalProfile.clientId" --output tsv)
+CLIENT_ID=$(az aks show --subscription $SUBID --resource-group $GROUPNAME --name $AKSNAME --query "servicePrincipalProfile.clientId" --output tsv)
 
  # Get the ACR registry resource id
-ACR_ID=$(az acr show --name $ACRNAME --resource-group $GROUPNAME --query "id" --output tsv)
+ACR_ID=$(az acr show --subscription $SUBID --name $ACRNAME --resource-group $GROUPNAME --query "id" --output tsv)
 
 # Create role assignment
-az role assignment create --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
+az role assignment create --subscription $SUBID --assignee $CLIENT_ID --role acrpull --scope $ACR_ID
 
 echo "your cluster is now ready to use, check the portal for more details"
 
